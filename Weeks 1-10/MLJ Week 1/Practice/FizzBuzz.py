3f931ca35fd3386cecbc7bae7ebebac8ad739c8d
jfashe <75461258+jfashe@users.noreply.github.com>
2025-06-13 04:32:55 -0500

    Post-W1 - FizzBuzz

--- Weeks 1-10/MLJ Week 1/Practice/FizzBuzz.py
+++ Weeks 1-10/MLJ Week 1/Practice/FizzBuzz.py
@@ -4,7 +4,17 @@
 # FizzBuzz.py
 #
 while True:
-    range = int(input("Enter a number from 15-1000! "))
-    
-    for i in (15, range+1):
-        print(i)
+    x = int(input("Enter a number from 1-1000! "))
+    if not 1 <= x <= 1000:
+        pass
+    else:
+        for i in range(1, x+1):
+            if i%15 == 0:
+                print("FizzBuzz")
+            elif i%3 == 0:
+                print("Fizz")
+            elif i%5 == 0:
+                print("Buzz")
+            else:
+                print(i)
+        break
