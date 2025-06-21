307496990665d0c445d775b7299cf83faf2d1fe6
jfashe <75461258+jfashe@users.noreply.github.com>
2025-06-21 04:25:34 -0500

    Post-W1 - Update 19

--- Weeks 1-10/MLJ Week 1/Practice/List_Comprehension.py
+++ Weeks 1-10/MLJ Week 1/Practice/List_Comprehension.py
@@ -1,14 +1,15 @@
 # List Comprehension!
 # "Show you can build a new list in one concise expression using Python’s list-comprehension syntax—optionally transforming and/or filtering items from an existing iterable instead of using an explicit loop."
-# https://www.youtube.com/watch?v=l8mWvDUwOt4
+# https://www.youtube.com/watch?v=l8mWvDUwOt4 (boo1)
+# 
 # Joshua Fashe
 # 6/21/2025 4:20am
-# Sum_A_List.py
+# List Comprehension.py
 
+nums,evens = [24, 13, 51, 64, 68, 111, 14, 560, 85 ], []
 
-mylist,sum = [4, 3, 3, -2], 0
+for num in nums:
+    if num % 2 == 0:
+        evens.append(num)
 
-for i in range(len(mylist)):
-    sum = (sum + int(mylist[i]))
-
-print(sum)
+print(evens)
