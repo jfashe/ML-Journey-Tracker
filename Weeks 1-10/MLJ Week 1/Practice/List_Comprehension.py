bd606a822b04bc9bb2332bf73374935e0fbeca3c
jfashe <75461258+jfashe@users.noreply.github.com>
2025-06-21 04:42:23 -0500

    Post-W1 - List_Comprehension

--- Weeks 1-10/MLJ Week 1/Practice/List_Comprehension.py
+++ Weeks 1-10/MLJ Week 1/Practice/List_Comprehension.py
@@ -1,11 +1,23 @@
 # List Comprehension!
 # "Show you can build a new list in one concise expression using Python’s list-comprehension syntax—optionally transforming and/or filtering items from an existing iterable instead of using an explicit loop."
 # https://www.youtube.com/watch?v=l8mWvDUwOt4 (boo1)
-# 
+# https://www.youtube.com/watch?v=YlY2g2xrl6Q
 # Joshua Fashe
 # 6/21/2025 4:20am
 # List Comprehension.py
 
+nums = [24, 13, 51, 64, 68, 111, 14, 560, 85 ]
+evens = [num for num in nums if num %2 == 0]
+# [expression for value in iterable if condition]
+# 'num' for num: 'num' is blank expression
+print(evens)
+
+squares = [i*i for i in range(1,11)]
+# [expression for value in (creates iterable)]
+print(squares)
+
+
+"""
 nums,evens = [24, 13, 51, 64, 68, 111, 14, 560, 85 ], []
 
 for num in nums:
@@ -13,3 +25,4 @@ for num in nums:
         evens.append(num)
 
 print(evens)
+"""
