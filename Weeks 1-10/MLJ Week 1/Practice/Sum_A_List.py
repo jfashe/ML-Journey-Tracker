dab579545069ead570f8c73da38c07383a75c525
jfashe <75461258+jfashe@users.noreply.github.com>
2025-06-21 03:42:43 -0500

    Post-W1 - Update 15 (Sum A List)

---
+++ Weeks 1-10/MLJ Week 1/Practice/Sum_A_List.py
@@ -0,0 +1,8 @@
+# Sum-A-List!
+# "Create a Python program that receives a collection of numeric values (e.g., a list) and produces their total sum as its output—nothing more, nothing less."
+# 
+# Joshua Fashe
+# 6/21/2025 3:41am
+# Sum_A_List.py
+
+
