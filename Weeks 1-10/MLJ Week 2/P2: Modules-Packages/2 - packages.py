9760a24c3ff9719a1a17086284561941e3ec2582
jfashe <75461258+jfashe@users.noreply.github.com>
2025-06-22 23:25:06 -0500

    Post-W2 - Update 7

---
+++ Weeks 1-10/MLJ Week 2/P2: Modules-Packages/2 - packages.py
@@ -0,0 +1,7 @@
+#https://www.youtube.com/watch?v=9z7gGUbAj5U
+# Python pip? - Bro Code
+# 
+
+# module -  a file containing code you want to include in your program
+#           use 'import' to include a module (built in or your own)
+#           useful to break up a large program reusable separate files
---
+++ Weeks 1-10/MLJ Week 2/P2: Modules-Packages/custommodule/main.py
@@ -0,0 +1,9 @@
+# https://www.youtube.com/watch?v=XcfxkHrHTVE
+import mymod
+
+print(mymod.square(3))
+print(mymod.cube(3))
+print(mymod.circumference(3))
+print(mymod.area(3))
+
+# It works
