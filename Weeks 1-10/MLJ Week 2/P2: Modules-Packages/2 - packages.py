b0a5054a03418008a3c8dded9e0bd81085bce73b
jfashe <75461258+jfashe@users.noreply.github.com>
2025-07-06 16:49:29 -0500

    Post-W2 - Update 10

--- Weeks 1-10/MLJ Week 2/P2: Modules-Packages/2 - packages.py
+++ Weeks 1-10/MLJ Week 2/P2: Modules-Packages/2 - packages.py
@@ -15,13 +15,18 @@ base_url = "https://pokeapi.co/api/v2/"
 def get_pokemon_info(name):
     url = f"{base_url}/pokemon/{name}"
     response = requests.get(url)
-    print(response)
 
     if response.status_code == 200:
         print("Data Retrieved!")
+        data = response.json()
+        return(data)    # data is now a dictionary (key:value) of pokemon info. 
+                        # and can be accessed by key.
     else:
         print(f"Failed to retrieve data {response.status_code}")
 
 pokemon_name = input("Which pokemon would you like to know about? ")
+pokemon_info = get_pokemon_info(pokemon_name)
 
-get_pokemon_info(pokemon_name)
+# If the info exists basically.
+if pokemon_info:
+    print(f"{pokemon_info["name"]}")
