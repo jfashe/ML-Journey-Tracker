87fd8db645e1d37fe5a96c9e3413b84395c24984
jfashe <75461258+jfashe@users.noreply.github.com>
2025-07-06 16:20:12 -0500

    Post-W2 - Update 8

--- Weeks 1-10/MLJ Week 2/P2: Modules-Packages/2 - packages.py
+++ Weeks 1-10/MLJ Week 2/P2: Modules-Packages/2 - packages.py
@@ -1,7 +1,18 @@
 #https://www.youtube.com/watch?v=9z7gGUbAj5U
+#https://www.youtube.com/watch?v=JVQNywo4AbU
 # Python pip? - Bro Code
+# Request API data using Python - Bro Code
 # 
 
-# module -  a file containing code you want to include in your program
+# pip -   Pyhon's package manager for packages and modules
 #           use 'import' to include a module (built in or your own)
 #           useful to break up a large program reusable separate files
+
+import requests
+
+base_url = "https://pokeapi.co/api/v2/"
+
+def get_pokemon_info(name):
+    pass
+
+pokemon_name = input("Which pokemon would you like to know about? ")
