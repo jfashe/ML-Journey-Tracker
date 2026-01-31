161c6a9170791f70335c909bcb02be73ff076e91
jfashe <75461258+jfashe@users.noreply.github.com>
2026-01-31 02:31:41 -0600

    Numpy shape

--- Andrew Ng ML Specialty/Week 1/1 - Model representation/modelrep.py
+++ Andrew Ng ML Specialty/Week 1/1 - Model representation/modelrep.py
@@ -2,10 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('./deeplearning.mplstyle')
 
+def clear():
+    print("------------------------------")
+def follow():
+    print("-----")
+
 # x_train is the input variable (size in 1000 square feet)
 # y_train is the target (price in 1000s of dollars)
 x_train = np.array([1.0, 2.0])
 y_train = np.array([300.0, 500.0])
 print(f"x_train = {x_train}")
 print(f"y_train = {y_train}")
+clear()
 
+# .shape returns the number of training examples as a tuple
+print(f"x_train.shape: {x_train.shape}")
+m = x_train.shape[0]        #.shape[0] returns just the first value in the tuple (actual # of training examples)
+print(f"Number of training examples is: {m}")
+follow()
+
+#as said in the notebook, we can use the len() func aswell
+print(f"Number of training examples is: {len(x_train)}")
