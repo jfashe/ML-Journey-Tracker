82dcb6fb18c40c2aa094c9786bde876143d23565
jfashe <75461258+jfashe@users.noreply.github.com>
2026-02-25 10:03:02 -0600

    Pipeline dataset created

--- Projects/Pipeline/main.py
+++ Projects/Pipeline/main.py
@@ -6,12 +6,16 @@ import joblib
 from sklearn.model_selection import train_test_split #flashcards?
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import StandardScale, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 # Loading Data
+data = pd.read_csv("vehicle_emissions.csv")
+data.head()
+data.info()
+
 # Create features and target variables
 # Split Categorial and Numerical Features
 # Combine the pipelines
