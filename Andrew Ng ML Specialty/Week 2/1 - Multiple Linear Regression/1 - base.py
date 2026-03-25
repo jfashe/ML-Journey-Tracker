762f800b21f6f07a0a623547704fff18da91ad0c
jfashe <75461258+jfashe@users.noreply.github.com>
2026-03-25 03:18:40 -0500

    Nunmpy - slicing

--- Andrew Ng ML Specialty/Week 2/1 - Multiple Linear Regression/1 - base.py
+++ Andrew Ng ML Specialty/Week 2/1 - Multiple Linear Regression/1 - base.py
@@ -9,3 +9,4 @@ a = np.random.random_sample(4); print(f"np.random.random_sample(4): a = {a}, a s
 # NumPy routines which allocate memory and fill with user specified values
 a = np.array([5,4,3,2]);  print(f"np.array([5,4,3,2]):  a = {a},     a shape = {a.shape}, a data type = {a.dtype}")
 a = np.array([5.,4,3,2]); print(f"np.array([5.,4,3,2]): a = {a}, a shape = {a.shape}, a data type = {a.dtype}")
+
